34305561a1673e3e2ba75e71e06424e5e78292ba
kjohnsen <kjohnsen6@gatech.edu>
2021-09-21 18:40:28 -0400

    fix test

--- pymod_test.py
+++ pymod_test.py
@@ -1,5 +1,8 @@
 from build.pymod import test_runtime_error
 
-test_runtime_error()
+try:
+    test_runtime_error()
+except RuntimeError as e:
+    print(e)
+    print('successfully caught error')
 
-print('success')
